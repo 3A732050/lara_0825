b92124830bf8ed61c467d83611e6d20e9c806f89
林駿綦 <r10101720307@gmail.com>
2020-11-15 20:37:34 +0800

    新增資料（create 方法，出現 mass assignment 警告）

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
+    /*
     $post = new Post();
     $post->title = 'test title';
     $post->content = 'test content';
     $post->save();
+    */
+    Post::create([
+        'title' => 'test title 2',
+        'content' => 'test content 2',
+    ]);
 });
 
 Route::get('index', [PostsController::class, 'index'])->name('posts.index');
