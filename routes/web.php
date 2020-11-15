0a7b2db63c8f8a6556f8c828bb6e64939ca06e8b
林駿綦 <r10101720307@gmail.com>
2020-11-15 20:35:10 +0800

    新增資料（new 建構式）

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    $post = new Post();
+    $post->title = 'test title';
+    $post->content = 'test content';
+    $post->save();
 });
 
 Route::get('index', [PostsController::class, 'index'])->name('posts.index');
